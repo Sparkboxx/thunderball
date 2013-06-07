6608e040f827cf7ccfcadef5d87672438a0c9da0
Michalis Agathos <magathos@nikhef.nl>
2013-06-07 22:58:26 +0200

    Added stuff to accelerometer header

--- wmp/accelerometer.h
+++ wmp/accelerometer.h
@@ -3,17 +3,21 @@
 
 class Accelerometer {
   // Values off the accellerometer
-  int x, y, z;
+  int acc_x, acc_y, acc_z;
 
   // In order to work with the Accellerometer, we have to set the initial value. After that
   // all measurements are relative to the values at t=0.
-  int x0, y0, z0;
+  int acc_x0, acc_y0, acc_z0;
 
   public:
     int calibrate_x(int);
     int calibrate_y(int);
     int calibrate_z(int);
     void calibrate(int, int, int);
+
+    void set_acc_x(int);
+    void set_acc_y(int);
+    void set_acc_z(int);
 };
 
 #endif
