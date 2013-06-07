7bfaa662f465656f6e34dd63a4e8a0d074473b84
Michalis Agathos <magathos@nikhef.nl>
2013-06-07 23:33:48 +0200

    implemented nunchuck

--- wmp/WMPNunchuckDataMapper.cpp
+++ wmp/WMPNunchuckDataMapper.cpp
@@ -28,10 +28,51 @@ http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Wii_Motion_Plus
 
 */
 
-void WMPNunchuckDataMapper::map(unsigned char data[]){
-};
 
 void WMPNunchuckDataMapper::set_wmp(WMP* wmp){
   pwmp = wmp;
 };
 
+void WMPNunchuckDataMapper::map(unsigned char new_data[]){
+  data = new_data;
+  parse_acc_x(); parse_acc_y(); parse_acc_z();
+  parse_butt_c(); parse_butt_z();
+  parse_analog_x(); parse_analog_y();
+};
+
+/*void WMPNunchuckDataMapper::initialize(){
+}
+*/
+
+// this should add bit 4 as bit 1 (verify)
+void WMPNunchuckDataMapper::parse_acc_x(){
+  accel->set_acc_x((data[2] << 2) + ((data[5] >> 3) & 0x0002)); 
+};
+
+// this should add bit 5 as bit 1 (verify)
+void WMPNunchuckDataMapper::parse_acc_y(){
+  accel->set_acc_y((data[3] << 2) + ((data[5] >> 4) & 0x0002) ); 
+};
+
+// this should add the bit 6 and 7 as bit 1 and 2 (verify)
+void WMPNunchuckDataMapper::parse_acc_z(){
+  accel->set_acc_z(((data[4] >> 1) << 3) + ((data[5] >> 5) & 0x0006)); 
+};
+
+/*
+void WMPNunchuckDataMapper::parse_butt_c(){
+  butt_c = ; 
+};
+
+void WMPNunchuckDataMapper::parse_butt_z(){
+  butt_z = ; 
+};
+
+void WMPNunchuckDataMapper::parse_analog_x(){
+  analog_x = ; 
+};
+
+void WMPNunchuckDataMapper::parse_analog_y(){
+  analog_y = ; 
+};
+*/
--- wmp/WMPNunchuckDataMapper.h
+++ wmp/WMPNunchuckDataMapper.h
@@ -1,17 +1,26 @@
 #ifndef WMPNUNCHUCKDATAMAPPER_H
 #define WMPNUNCHUCKDATAMAPPER_H
 
-#include <wmp.h>
+#include "wmp.h"
+#include "accelerometer.h"
 
 class WMP;
 
 class WMPNunchuckDataMapper {
   unsigned char *data;
   WMP* pwmp;
+  Accelerometer* accel;
 
   public:
     void map(unsigned char*);
-    void set_wmp(WMP*);
+    void parse_wmp(WMP*);
+    void parse_acc_x();
+    void parse_acc_y();
+    void parse_acc_z();
+    void parse_butt_c();
+    void parse_butt_z();
+    void parse_analog_x();
+    void parse_analog_y();
 };
 
 #endif
